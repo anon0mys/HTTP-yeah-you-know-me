74e68114b101c411173117417a2931a427536fdb
Evan Wheeler <anon0mys87@gmail.com>
2018-02-16 11:08:23 -0700

    Force Error tests

--- test/paths/force_error_test.rb
+++ test/paths/force_error_test.rb
@@ -1,8 +1,21 @@
 require './test/test_helper'
-require './lib/paths/shutdown'
+require './lib/paths/force_error'
 
 # Tests shutdown path
-class ShutdownTest < Minitest::Test
+class ForceErrorTest < Minitest::Test
   include TestHelper
 
+  def test_it_outputs_error_code
+    error = ForceError.new(1)
+    body = error.body(stub_diagnostics('/force_error'))
+
+    assert body.include?('500 Internal Server Error')
+  end
+
+  def test_headers_contains_error_code
+    error = ForceError.new(1)
+    headers = error.headers(1)
+
+    assert headers[0].include?('500 Internal Server Error')
+  end
 end
